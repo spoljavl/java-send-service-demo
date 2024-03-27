7cb138639d8feee3856a82299c8d3c987400460e
Vlatko Spoljaric <vlatko.spoljaric@coinme.com>
2024-03-27 21:37:51 +0100

    JSSD-005 Create currency and iban validators
    CurrencyValidator added
    IbanValidator added
    Currencies added

--- src/test/java/com/endava/javacommunity/sendservice/validators/CurrencyValidatorTest.java
+++ src/test/java/com/endava/javacommunity/sendservice/validators/CurrencyValidatorTest.java
@@ -3,22 +3,35 @@ package com.endava.javacommunity.sendservice.validators;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class CurrencyValidatorTest {
+
+  private static CurrencyValidator currencyValidator;
+
+  @BeforeAll
+  static void init() {
+    currencyValidator = Mockito.mock(CurrencyValidator.class);
+  }
 
   @Test
   public void shouldValidateEURCurrency() {
     final String currencySymbol = "EUR";
 
-    assertTrue(CurrencyValidator.isSupportedCurrency(currencySymbol));
+    Mockito.when(currencyValidator.isSupportedCurrency(currencySymbol)).thenReturn(true);
+
+    assertTrue(currencyValidator.isSupportedCurrency(currencySymbol));
   }
 
   @Test
   public void shouldNotValidateFakeCurrency() {
     final String currencySymbol = "Fake";
 
-    assertFalse(CurrencyValidator.isSupportedCurrency(currencySymbol));
+    Mockito.when(currencyValidator.isSupportedCurrency(currencySymbol)).thenReturn(false);
+
+    assertFalse(currencyValidator.isSupportedCurrency(currencySymbol));
   }
 
 }
